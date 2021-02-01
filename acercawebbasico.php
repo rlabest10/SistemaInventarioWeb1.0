f71ac8f4ea7ba4afd9fa6c1863e19a2e492cb850
rlabest10 <roger.arana@gmail.com>
2021-02-01 09:54:14 -0600

    Inicio de carga Files

---
+++ acercawebbasico.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca Web Basico</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>    <!-- Aqui inicial el NavBar -->
+    <nav>
+        <div class="inv-image">
+            <img src="img/inventario/image12.jpg" alt="" height="60px" border:"1">
+        </div>
+        <ul class="nav-options">
+            <li> <a href="indexwebbasico.php"> Inicio </a> </li>
+            <li> <a href="#">Productos </a></li>
+            <li> <a href="#">Consultas </a></li>
+            <li> <a href="#">Reportes </a></li>
+            <li> <a href="acercawebbasico.php">Acerca... </a></li>
+        </ul>
+    </nav>
+    <!-- Aqui Termina el NavBar -->
+
+    <h1>Acerca del Sistema</h1>
+</body>
+</html>
---
+++ indexwebbasico.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diseño de Basico</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/mobile.css">
+</head>
+<body>
+    <!-- Aqui inicial el NavBar -->
+    <nav>
+        <div class="inv-image">
+            <img src="img/inventario/image12.jpg" alt="" height="60px" border:"1">
+        </div>
+        <ul class="nav-options">
+        <li> <a href="indexwebbasico.php"> Inicio </a> </li>
+            <li> <a href="#">Productos </a></li>
+            <li> <a href="#">Consultas </a></li>
+            <li> <a href="#">Reportes </a></li>
+            <li> <a href="acercawebbasico.php">Acerca... </a></li></ul>
+    </nav>
+    <!-- Aqui Termina el NavBar -->
+    <h1>Inicio del Sistema</h1>
+    
+    <div class="main_section">
+    
+    </div>
+
+</body>
+</html>
